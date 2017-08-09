a4f78e571e928edccba432e145c0154a81a5c6dd
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-08-08 21:41:42 -0300

    Iniciando repositorio

---
+++ Mercado do zé/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mercado_do_zé.MvcApplication" Language="C#" %>
---
+++ Mercado do zé/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mercado_do_zé.Startup))]
+namespace Mercado_do_zé
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
