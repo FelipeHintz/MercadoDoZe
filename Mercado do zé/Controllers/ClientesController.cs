39af2dfdc6ef6801c10de338fc200f6df47e032a
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-08-17 19:25:28 -0300

    alteracoes lokas

---
+++ Mercado do zé/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mercado_do_zé.Models;
+
+namespace Mercado_do_zé.Controllers
+{
+    public class ClientesController : Controller
+    {
+        //
+        // GET: /Clientes/
+        public ActionResult Random()
+        {
+            var cliente = new Cliente() { nome = "Zé" };
+            return View(cliente);
+        }
+	}
+}
---
+++ Mercado do zé/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mercado_do_zé.Models
+{
+    public class Cliente
+    {
+
+        public int id { get; set; }
+
+        public string nome { get; set;}
+
+    }
+}
