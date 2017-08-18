e14706dca26b8e19afea40acd171f11c30d02ec5
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-08-17 21:46:49 -0300

    commit

--- Mercado do zé/Controllers/HomeController.cs
+++ Mercado do zé/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Mercado_do_zé.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Página de contato";
 
             return View();
         }
