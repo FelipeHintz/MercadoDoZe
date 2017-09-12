7face6c97a70cdb7e1d9e68db82a00a9ca74bee9
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-09-12 20:00:15 -0300

    Alterado controller dos produtos

--- Mercado do zé/Controllers/ProdutosController.cs
+++ Mercado do zé/Controllers/ProdutosController.cs
@@ -8,33 +8,36 @@ using System.Web.Mvc;
 
 namespace Mercado_do_zé.Controllers
 {
-	public class ProdutosController : Controller
-	{
-		public IEnumerable<Produto> GetProdutos()
-		{
-			return new List<Produto>
-			{
-				new Produto {Nome = "Sabão", Id = 1, Descrição = "Tem cheiro", Quantidade = 100, Valor = 2.50, EnderecoImagem = "teste.gif"},
-				new Produto {Nome = "Pão", Id = 2, Descrição = "Duro igual pedra", Quantidade = 50, Valor = 0.25, EnderecoImagem = "teste.gif"}
-			};
-		}
+    public class ProdutosController : Controller
+    {
+        private ApplicationDbContext _context;
 
-		public ActionResult Detalhes(int id)
-		{
-            var produto = GetProdutos().SingleOrDefault(c => c.Id == id);
+        public ProdutosController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            return View(produtos);
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var produto = _context.Produtos.SingleOrDefault(c => c.Id == id);
 
             if (produto == null)
                 return HttpNotFound();
 
             return View(produto);
-		}
+        }
 
-		public ActionResult Index()
-		{
-			var produtos = GetProdutos();
-
-			return View(produtos);
-		}
-
-	}
+    }
 }
