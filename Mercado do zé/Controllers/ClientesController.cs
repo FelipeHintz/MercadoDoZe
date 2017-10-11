e0ef656f56a026e8aab7d6334ce1999f7b36ca01
Felipe Hintz <felipe.hintz@catolicasc.org.br>
2017-10-10 21:49:06 -0300

    commit maneiro dos bagulho maneiro que são maneiro

--- Mercado do zé/Controllers/ClientesController.cs
+++ Mercado do zé/Controllers/ClientesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Mercado_do_zé.Models;
+using Mercado_do_zé.ViewModels;
 using System.Data.Entity;
 
 namespace Mercado_do_zé.Controllers
@@ -38,6 +39,55 @@ namespace Mercado_do_zé.Controllers
 
 			return View(cliente);
 		}
+
+        public ActionResult Editar(int id)
+        {
+            var cliente = _context.Clientes.SingleOrDefault(c => c.Id == id);
+
+            if (cliente == null)
+                return HttpNotFound();
+
+            var viewModel = new ClienteEditarViewModel
+            {
+                Cliente = cliente
+            };
+            
+            return View("Editar", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Cliente cliente) // recebemos um cliente
+        {
+            if (cliente.Id == 0)
+            {
+                // armazena o cliente em memória
+                _context.Clientes.Add(cliente);
+            }
+            else
+            {
+                var customerInDb = _context.Clientes.Single(c => c.Id == cliente.Id);
+
+                customerInDb.Nome = cliente.Nome;
+                customerInDb.EnderecoImagem = cliente.EnderecoImagem;
+                customerInDb.Descricao = cliente.Descricao;
+            }
+
+            // faz a persistência
+            _context.SaveChanges();
+            // Voltamos para a lista de clientes
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Novo()
+        {
+            var viewModel = new ClienteEditarViewModel
+            {
+
+            };
+
+            return View("Editar", viewModel);
+        }
+
 
 	}
 }
---
+++ Mercado do zé/ViewModels/ClienteEditarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Mercado_do_zé.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mercado_do_zé.ViewModels
+{
+    public class ClienteEditarViewModel
+    {
+        public Cliente Cliente { get; set; }
+
+        public String Titulo
+        {
+            get
+            {
+                if (Cliente != null && Cliente.Id != 0) 
+                {
+                    return "Editar cliente";
+                }
+                else {
+                    return "Cadastrar novo cliente";
+                }
+            }
+        }
+	}
+}
